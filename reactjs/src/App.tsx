c137c07dcb434d966cdcc464fa92ddd853f088a0
AdrianoAnimus <adrianoanimus@gmail.com>
2022-07-18 19:53:18 -0300

    Creating form field

--- reactjs/src/App.tsx
+++ reactjs/src/App.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline, Grid, ThemeProvider} from "@mui/material";
+import { CssBaseline, Grid, MenuItem, ThemeProvider} from "@mui/material";
 import { Navbar } from "./components/Navbar";
 import theme from './theme';
 
@@ -8,7 +8,16 @@ function App() {
       <CssBaseline/>
       <Navbar/>
       <Grid container>
-        <Grid item xs={12} sm={3}></Grid>
+        <Grid item xs={12} sm={3}>
+          <form>
+            <select>
+              <MenuItem value="">
+                <em>Select one route</em>
+
+              </MenuItem>
+            </select>
+          </form>
+        </Grid>
         <Grid item xs={12} sm={9}></Grid>
       </Grid>
     </ThemeProvider>
