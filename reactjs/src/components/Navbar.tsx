4f5d278e3c75e058286aae865aefa683ea3e47b7
AdrianoAnimus <adrianoanimus@gmail.com>
2022-07-18 19:12:11 -0300

    Create Icon Button

--- reactjs/src/components/Navbar.tsx
+++ reactjs/src/components/Navbar.tsx
@@ -1,10 +1,13 @@
-import { AppBar,Button, CssBaseline, ThemeProvider, Toolbar, Typography } from "@mui/material";
+import { AppBar,Button, CssBaseline, IconButton, ThemeProvider, Toolbar, Typography } from "@mui/material";
 import DriveEtaIcon from '@mui/icons-material/DriveEta';
 
 export function Navbar() {
     return(
         <AppBar>
         <Toolbar>
+          <IconButton color="inherit" edge="start">
+
+          </IconButton>
           <Typography variant="h6">Full Cycle Delivery</Typography>
         </Toolbar>
       </AppBar>
